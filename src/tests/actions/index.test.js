0f02908c9ef2411c96fae4c29cadbe535805a09d
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-07-20 09:47:53 +0100

    added test for my actions

---
+++ src/tests/actions/index.test.js
@@ -0,0 +1,31 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { FETCH_RECIPE, CLEAR_DATA } from '../../actions/index';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('async actions', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({});
+  });
+
+  it('dispatches the correct action and payload', () => {
+    // const expectedActions = [
+    //   {
+    //     type: 'FETCH_RECIPE',
+    //     payload: 1,
+    //   },
+    // ];
+
+    store.dispatch(FETCH_RECIPE());
+    expect(store.getActions()).toMatchSnapshot();
+  });
+
+  it('dispatches the correct action and payload', () => {
+    store.dispatch(CLEAR_DATA());
+    expect(store.getActions()).toMatchSnapshot();
+  });
+});
